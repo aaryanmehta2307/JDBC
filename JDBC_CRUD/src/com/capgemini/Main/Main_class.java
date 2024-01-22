0bf91dc31080849f922deb4b40c8ec4022e92cca
aaryanmehta2307 <aaryanmehta619@gmail.com>
2024-01-22 14:32:50 +0530

    first-commit

---
+++ JDBC_CRUD/src/com/capgemini/Main/Main_class.java
@@ -0,0 +1,11 @@
+package com.capgemini.Main;
+
+public class Main_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+
+	}
+
+}
