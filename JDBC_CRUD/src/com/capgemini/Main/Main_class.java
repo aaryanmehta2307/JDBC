c1e2f50a10ded25ede34aa50dce974a94ea7e3bf
aaryanmehta2307 <aaryanmehta619@gmail.com>
2024-01-29 21:45:04 +0530

    connection-established

--- JDBC_CRUD/src/com/capgemini/Main/Main_class.java
+++ JDBC_CRUD/src/com/capgemini/Main/Main_class.java
@@ -1,11 +1,19 @@
 package com.capgemini.Main;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main_class {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
-
+		
+		Connection con=null;
+		Class.forName("com.mysql.cj.jdbc.Driver");// load and register the driver
+		con=DriverManager.getConnection("jdbc:mysql://localhost:3306/cuims","root","1234");
+		System.out.println(con.getClass().getName());
+//		System.out.println("Connection Established");
 	}
 
 }
--- JDBC_CRUD/src/module-info.java
+++ JDBC_CRUD/src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module JDBC_CRUD {
+	requires java.sql;
 }
