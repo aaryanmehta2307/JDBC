0aa41a0cad07588505b67ffc6f10a170145d51dc
aaryanmehta2307 <aaryanmehta619@gmail.com>
2024-02-04 22:02:25 +0530

    EVERY-error-countered-fully-completed

--- JDBC_CRUD/src/com/capgemini/Main/OperationClass.java
+++ JDBC_CRUD/src/com/capgemini/Main/OperationClass.java
@@ -16,7 +16,7 @@ public class OperationClass {
 		System.out.println("Enter student UID:");
 		String StudentUID=sc.next();
 		System.out.println("Enter student Contact:");
-		 int  StudentContact=sc.nextInt();
+		 String  StudentContact=sc.next();
 		System.out.println("Enter student Name:");
 		String StudentName=sc.next();
 		System.out.println("Enter student Course: ");
@@ -39,7 +39,7 @@ public class OperationClass {
 		psmt2.setInt(1,count );
 	
 		psmt2.setString(2,StudentUID );
-		psmt2.setInt(3,StudentContact);		
+		psmt2.setString(3,StudentContact);		
 		psmt2.setString(4,StudentName);
 		psmt2.setString(5,Studentccourse);
 		
@@ -71,7 +71,7 @@ public class OperationClass {
 		ResultSet rs=psmt1.executeQuery();
 		if(rs.next()) {
 			System.out.println("Student UID:"+rs.getString(2));
-			System.out.println("Student Contact:"+rs.getInt(3));
+			System.out.println("Student Contact:"+rs.getString(3));
 			System.out.println("Student Name:"+rs.getString(4));
 			System.out.println("Student course:"+rs.getString(5));
 			
@@ -79,23 +79,65 @@ public class OperationClass {
 		else {
 			System.out.println("Record not found");
 		}
-//		System.out.println("Enter the attribute you want to change \n 1. Contact \n 2.Name \n 3. Course  ");
+		System.out.println("Enter the attribute you want to change \n 1. Contact \n 2.Name \n 3. Course  ");
+		int updatereply=sc.nextInt();
+		switch(updatereply) {
 		
-		System.out.println("Enter updated course:");
-		String updatecourse=sc.next();
-		
-		String updateQuery="update student set Studentccourse=? where StudentUID=?";
-		PreparedStatement psmt2=con.prepareStatement(updateQuery);
-		psmt2.setString(1, updatecourse);
-		psmt2.setString(2, StudentUID);
-		 
-		int status=psmt2.executeUpdate();
-		if(status>0) {
-			System.out.println("Record updated successfully");
+		case 1:
+			System.out.println("Enter updated Contact:");
+			String updatecourse=sc.next();
 			
+			String updateQuery="update student set Studentcontact=? where StudentUID=?";
+			PreparedStatement psmt2=con.prepareStatement(updateQuery);
+			psmt2.setString(1, updatecourse);
+			psmt2.setString(2, StudentUID);
+			 
+			int status=psmt2.executeUpdate();
+			if(status>0) {
+				System.out.println("Record updated successfully");
+				
+			}	
+			psmt2.close();
+			break;
+		case 2:
+			System.out.println("Enter updated Name:");
+			String updateName=sc.next();
+			
+			String updateQuery1="update student set StudentName=? where StudentUID=?";
+			PreparedStatement psmt3=con.prepareStatement(updateQuery1);
+			psmt3.setString(1, updateName);
+			psmt3.setString(2, StudentUID);
+			 
+			int setstatus=psmt3.executeUpdate();
+			if(setstatus>0) {
+				System.out.println("Record updated successfully");
+				
+			}	
+			psmt3.close();
+			break;
+		case 3:
+			System.out.println("Enter updated course:");
+			String updatecourse1=sc.next();
+			
+			String updateQuery2="update student set Studentccourse=? where StudentUID=?";
+			PreparedStatement psmt4=con.prepareStatement(updateQuery2);
+			psmt4.setString(1, updatecourse1);
+			psmt4.setString(2, StudentUID);
+			 
+			int status4=psmt4.executeUpdate();
+			if(status4>0) {
+				System.out.println("Record updated successfully");
+				
+			}	
+			psmt4.close();
+			break;
+			default:
+				System.out.println("Invalid input");
+				break;
 		}
+		
+		
 		psmt1.close();
-		psmt2.close();
 		con.close();
 		
 	}
@@ -112,7 +154,7 @@ public class OperationClass {
 		ResultSet rs=psmt1.executeQuery();
 		if(rs.next()) {
 			System.out.println("Student UID:"+rs.getString(2));
-			System.out.println("Student Contact:"+rs.getInt(3));
+			System.out.println("Student Contact:"+rs.getString(3));
 			System.out.println("Student Name:"+rs.getString(4));
 			System.out.println("Student course:"+rs.getString(5));
 			
@@ -154,7 +196,7 @@ public class OperationClass {
 			ResultSet rs=psmt1.executeQuery();
 			if(rs.next()) {
 				System.out.println("Student UID:"+rs.getString(2));
-				System.out.println("Student Contact:"+rs.getInt(3));
+				System.out.println("Student Contact:"+rs.getString(3));
 				System.out.println("Student Name:"+rs.getString(4));
 				System.out.println("Student course:"+rs.getString(5));
 				
@@ -173,7 +215,7 @@ public class OperationClass {
 			ResultSet rs=psmt1.executeQuery();
 			while(rs.next()) {
 				System.out.println("Student UID:"+rs.getString(2));
-				System.out.println("Student Contact:"+rs.getInt(3));
+				System.out.println("Student Contact:"+rs.getString(3));
 				System.out.println("Student Name:"+rs.getString(4));
 				System.out.println("Student course:"+rs.getString(5));
 				
